2fe13f45b468978daadc56d3c98154c3d0441f0a
Itumeleng Chabana <chabanaitumeleng@gmail.com>
2025-11-04 10:44:54 +0200

    Migrate from MongoDB to Firestore2

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,14 +1,12 @@
 const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
-let serviceAccount;
+// Check if running in production
+const serviceAccountPath = process.env.NODE_ENV === 'production'
+  ? '/etc/secrets/serviceAccountKey.json'
+  : path.join(__dirname, 'serviceAccountKey.json');
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // Production: use environment variable
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  // Development: use local file
-  serviceAccount = require('./serviceAccountKey.json');
-}
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
